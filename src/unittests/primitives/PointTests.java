f2118b3cb0264269059c23c9c8479924b1c8e805
PC <PC@DESKTOP-PUDL4CT.local>
2025-03-28 18:09:07 +0300

    final Point tests

--- src/unittests/primitives/PointTests.java
+++ src/unittests/primitives/PointTests.java
@@ -25,20 +25,24 @@ class PointTests {
 		
 		
 		// ============================ Equivalence Partitions Tests ============================
-		assertEquals(poi.add(vec),new Point(3,5,2),"wrong resulte for adding vector to point");
+		assertEquals(poi.add(vec),new Point(3,5,-2),"wrong resulte for adding vector to point");
 	
 		// ============================ Boundary Values Tests ============================
-		assertEquals(poi.add(vec2),Point.ZERO,"wrong resulte for adding vector to point");
-		
-		
+		assertEquals(poi.add(vec2),Point.ZERO,"wrong resulte zero for adding vector to point");	
 	}
 
 	/**
 	 * Test method for {@link primitives.Point#subtract(primitives.Point)}.
+	 * No have Boundary Values Tests because no zero vector was exception. 
 	 */
 	@Test
 	void testSubtract() {
-		fail("Not yet implemented");
+		Point poi = new Point(2,3,-5);
+		Point poi2 = new Point(1,2,3);
+	
+		// ============================ Equivalence Partitions Tests ============================
+		assertEquals(poi.subtract(poi2),new Vector(1,1,-8),"wrong resulte vector for subtract point to point");
+
 	}
 
 	/**
@@ -46,7 +50,9 @@ class PointTests {
 	 */
 	@Test
 	void testDistanceSquared() {
-		fail("Not yet implemented");
+		Point poi = new Point(2,3,-5);
+		Point poi2 = new Point(1,2,3);
+		assertEquals(poi.distanceSquared(poi2),66 ,"wrong resulte distance squared"); //8.12403840463596 Math.sqrt(66)
 	}
 
 	/**
@@ -54,7 +60,9 @@ class PointTests {
 	 */
 	@Test
 	void testDistance() {
-		fail("Not yet implemented");
+		Point poi = new Point(2,3,-5);
+		Point poi2 = new Point(1,2,3);
+		assertEquals(poi.distance(poi2),Math.sqrt(66) ,"wrong resulte distance squared"); //8.12403840463596 
 	}
 
 }
