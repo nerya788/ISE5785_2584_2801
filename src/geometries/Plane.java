98c833822317df02cbda25049242138ab28bd3cc
YehudaCopperman <yehuda.copperman@gmail.com>
2025-04-20 16:58:34 +0300

    implemented the method findIntersections for the Plane class. also added an Empty method and test method of findIntersections for Triangle class to be completed later..

--- src/geometries/Plane.java
+++ src/geometries/Plane.java
@@ -11,7 +11,9 @@ import primitives.*;
 public class Plane extends Geometry {
 	private final Point head; // A point on the plane
 	private final Vector direction; // The normal vector to the plane
-
+	
+	public static final double Zero = 0.0;
+	
 	/**
 	 * Constructs a plane from three points in 3D space.
 	 * 
@@ -46,8 +48,37 @@ public class Plane extends Geometry {
 		return direction;
 	}
 
+	/**
+     * Finds the intersection points between a given {@link Ray} and the plane.
+     * 
+     * If there are intersection points, they are calculated and returned as a list.
+     * If there are no intersection points (the ray does not intersect the plane),
+     * this method returns {@code null}.
+     * if the ray head and the plane head is equals the result is an empty list.
+     * if the ray is a subset of the plane (infinity points) then returns an empty list.
+     *
+     * @param ray The {@link Ray} to check for intersections with the plane
+     * @return A list of intersection points as {@link Point} objects, or {@code null} if none exist
+     */
 	@Override
 	public List<Point> findIntersections(Ray ray) {
-		return null;
-	}
+		if (ray.getHead().equals(head))
+			return List.of();
+		Vector v = ray.getHead().subtract(head);
+		if (Util.isZero(ray.getDirection().dotProduct(direction))) {
+			if (Util.isZero(v.dotProduct(direction)))
+				return List.of();
+			return null;
+		}
+		if (Util.isZero(v.dotProduct(direction)))
+			return List.of();
+		Vector v1 = head.subtract(ray.getHead());
+		double numerator = v1.dotProduct(direction);
+		double denominator = ray.getDirection().dotProduct(direction);
+		double t = Util.alignZero(numerator / denominator);
+		if (t <= Zero)
+			return null;
+		Point intersection = ray.getHead().add(ray.getDirection().scale(t));
+		return List.of(intersection);
+	}	
 }
