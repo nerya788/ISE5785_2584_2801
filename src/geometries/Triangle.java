f994ad4522168828996ae4317968a97a32b99218
YehudaCopperman <yehuda.copperman@gmail.com>
2025-04-21 00:03:54 +0300

    Completed Triangle.findIntersections and the test method.

--- src/geometries/Triangle.java
+++ src/geometries/Triangle.java
@@ -4,8 +4,6 @@ import primitives.*;
 
 import java.util.List;
 
-import geometries.*;
-
 /**
  * Represents a triangle in 3D space. This class extends Polygon, assuming a
  * triangle is a specific case of a polygon with three sides.
@@ -23,6 +21,35 @@ public class Triangle extends Polygon {
 	
 	@Override
 	public List<Point> findIntersections(Ray ray) {
+		final List<Point> planeIntersections = plane.findIntersections(ray);
+		if (planeIntersections == null)
+			return null;
+		
+		final Point planeIntersection = planeIntersections.getFirst();
+		
+		final Point a = this.vertices.get(0);
+		final Point b = this.vertices.get(1);
+		final Point c = this.vertices.get(2);
+		
+		final Vector v0 = c.subtract(a);
+		final Vector v1 = b.subtract(a);
+		final Vector v2 = planeIntersection.subtract(a);
+		
+		final double d00 = v0.dotProduct(v0);
+		final double d01 = v0.dotProduct(v1);
+		final double d11 = v1.dotProduct(v1);
+		final double d20 = v2.dotProduct(v0);
+		final double d21 = v2.dotProduct(v1);
+		
+		final double denom = d00 * d11 - d01 * d01;
+		
+		final double v = (d11 * d20 - d01 * d21) / denom;
+		final double w = (d00 * d21 - d01 * d20) / denom;
+		final double u = 1.0 - v - w;
+		
+		if (u > 0 && v > 0 && w > 0)
+			return planeIntersections;
+		
 		return null;
 	}
 }
