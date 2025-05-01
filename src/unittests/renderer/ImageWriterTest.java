8aea6fc75f8bec3936bb41104e95bea6eba6f8a1
nerya788 <neryh@g.jct.ac.il>
2025-05-01 15:52:05 +0300

    ImageWriterTest

--- src/unittests/renderer/ImageWriterTest.java
+++ src/unittests/renderer/ImageWriterTest.java
@@ -22,11 +22,29 @@ import primitives.Color;
 class ImageWriterTest {
 
 	/**
-	 * Test method for {@link renderer.ImageWriter#ImageWriter(int, int)}.
+	 * Test method for {@link renderer.ImageWriter#ImageWriter(int nX, int nY)}.
 	 */
 	@Test
 	void testImageWriter() {
-		ImageWriter image(800,500,BufferedImage.TYPE_INT_RGB);
-	}
+		   ImageWriter images = new ImageWriter(500,800);
+		   for (int i = 0; i < 500; i++) {
+			   for (int j = 0; j < 800; j++) {
+				   
+				   if (i % 50 == 0)
+					   images.writePixel(i, j, new Color(225,0,0));
+				   else if (j % 50 == 0)
+					   images.writePixel(i, j, new Color(225,0,0));
+				   else
+					   images.writePixel(i, j, new Color(225,225,0));
+			   }
+		   }
+		   
+		   
+		   for (int i = 0; i < 500; i++) 
+			   images.writePixel(i, 799, new Color(225,0,0));
+		   for (int j = 0; j < 800; j++)
+			   images.writePixel(499, j, new Color(225,0,0));
 
+		   images.writeToImage("final");
+	}
 }
